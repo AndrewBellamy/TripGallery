51701a87962bbcf9ad0fa282e41bd93bd2dc0ce7
AndrewBellamy <andymanbellamy@gmail.com>
2017-05-03 17:42:22 +0930

    initial commit

---
+++ TripGallery/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TripGallery
+//
+//  Created by Andrew Bellamy on 3/5/17.
+//  Copyright © 2017 Andrew Bellamy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //Just started
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
